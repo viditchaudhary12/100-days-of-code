e725e3c5e19b49f03511eea27f2ee5edff203bb4
vidit chaudhary <viditchaudhary302@gmail.com>
2026-09-09 20:00:58 +0530

    Create day1_2.c

---
+++ day1_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    scanf("%d %d", &num1, &num2);
+    printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d\n", 
+           num1 + num2, num1 - num2, num1 * num2, num1 / num2);
+
+    return 0;
+}
