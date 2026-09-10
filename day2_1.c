c63a60b65be30c540ef9e2eba47ba8af7d7d76f6
vidit chaudhary <viditchaudhary302@gmail.com>
2026-09-10 23:21:11 +0530

    Create day2_1.c

---
+++ day2_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int length, breadth;
+
+    scanf("%d %d", &length, &breadth);
+    printf("Area=%d, Perimeter=%d\n", length * breadth, 2 * (length + breadth));
+
+    return 0;
+}
