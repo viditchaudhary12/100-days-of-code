a630b3b1a045ecbe264ee71a7a0bb9f27513dea0
vidit chaudhary <viditchaudhary302@gmail.com>
2026-09-13 18:55:55 +0530

    Create day10_1.c

---
+++ day10_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c) {
+        printf("Equilateral\n");
+    } else if (a == b || b == c || a == c) {
+        printf("Isosceles\n");
+    } else {
+        printf("Scalene\n");
+    }
+
+    return 0;
+}
