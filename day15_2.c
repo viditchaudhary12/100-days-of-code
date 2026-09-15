1c7af28bd4532ba261f2ba35da3124ec1094efc9
vidit chaudhary <viditchaudhary302@gmail.com>
2026-09-15 23:40:32 +0530

    Create day15_2.c

---
+++ day15_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num, rev = 0, rem;
+
+    scanf("%d", &num);
+
+    while (num != 0) {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num /= 10;
+    }
+
+    printf("%d\n", rev);
+
+    return 0;
+}
